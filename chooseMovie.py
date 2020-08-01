63289e617e0b95224f3d1aa55e14c52c71ba38af
Luthfor Khan <luthfork@gmail.com>
2020-07-31 23:14:29 -0400

    I think i completed the program. [N/A] represents movies that are not on the Top 100 Movie Poster and are added from IMDB

--- chooseMovie.py
+++ chooseMovie.py
@@ -1,5 +1,6 @@
 import random
 
+global run
 run = True
 
 def options():
@@ -20,23 +21,24 @@ def options():
 
     return usrChoice
 
-def SelectRandom(movieList,watchedList):
+def SelectRandom(movieList, watchedList):
     randomMovie = random.choice(movieList)
     print("The movie selected is: ", randomMovie)
     ans = input("Select this movie (y/n)?\n")
-    
+    chosenMovie = randomMovie.rstrip('\n')
+
     if ans == 'n':
         print("Rerolling for a new movie")
-        SelectRandom(movieList)
+        SelectRandom(movieList, watchedList)
     elif ans == 'y':
-        print("Added ", randomMovie, " to watched list.")
+        print("Added", chosenMovie, " to watched list.")
         watchedList.append(randomMovie)
 
-        print("Removed ", randomMovie," from movie list.")
+        print("Removed ", chosenMovie," from movie list.")
         movieList.remove(randomMovie)
     else:
         print("Invalid Option, Rerolling movie")
-        SelectRandom(movieList)
+        SelectRandom(movieList, watchedList)
 
 def unwatched(unwatchedList):
     size = len(unwatchedList)
@@ -64,7 +66,7 @@ def addList(movieFile):
     
     print(movie," has been added to movies.txt file")
 
-def main(escape): 
+def main(): 
     print("Welcome to the Movie Selector\n")
 
     choice = options()
@@ -74,8 +76,8 @@ def main(escape):
     fileOUT = "watched.txt"
 
     # input text files of movie list and watch list
-    movies = open(fileIN, "a+")
-    watched = open(fileOUT, "a+")
+    movies = open(fileIN, "r+")
+    watched = open(fileOUT, "r+")
 
     # inputing movies from file to container
     print("Importing movie names from: ", fileIN,"\n")
@@ -98,30 +100,77 @@ def main(escape):
     print("Import Complete\n")
 
     if choice == 1:
-        SelectRandom(movieList, watchedList)
+        SelectRandom(movieList,watchedList)
+        
+        print("Saving progress and writing to files.\n")
+        
+        # closing open file streams
+        movies.close()
+        watched.close()
+        
+        # open write up files
+        movie = open(fileIN, "w+")
+        watch = open(fileOUT, "w+")
+
+        # write out list into files
+        for title in movieList:
+            movie.write(title)
+
+        for title in watchedList:
+            watch.write(title) 
+
+        # close files streams
+        movie.close()
+        watch.close()
+
     elif choice == 2:
         unwatched(movieList)
+
+        # closing open file streams
+        movies.close()
+        watched.close()
     elif choice == 3:
         watched(watchedList)
+
+        # closing open file streams
+        movies.close()
+        watched.close()
     elif choice == 4:
         addList(movies)
+
+        # closing open file streams
+        movies.close()
+        watched.close()
     elif choice == 5:
-        escape = False
-        print(escape)
+        # closing open file streams
+        movies.close()
+        watched.close()
+        
+        # open write up files
+        movie = open(fileIN, "w+")
+        watch = open(fileOUT, "w+")
+
+        # changing bool value for run for the while loop
+        global run
+        run = False
+
+        # display closing messages
+        endGame()
+
+        # write out list into files
+        for title in movieList:
+            movie.write(title)
+
+        for title in watchedList:
+            watch.write(title) 
+
+        # close files streams
+        movie.close()
+        watch.close()
+
+def endGame():
+    print("Saving changes to associated files.")
+    print("Exiting Program.\n")
 
 while run:
-    main(run)
-
-
-'''for k in movieList:
-    print(k.rstrip('\n'))
-
-print("test of writing to file")
-writeFile = open("wat.txt", "w+")
-
-movieList[1] = "this is a new line added by the program to the text file\n"
-
-for tit in movieList:
-    writeFile.write(tit)
-
-writeFile.close()'''
+    main()
